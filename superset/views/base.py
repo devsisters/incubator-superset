2579c7cb1af88aff640777387b5f48acc7fc9b54
김태겸 <55680952+kimtkyeom@users.noreply.github.com>
2020-01-16 17:20:38 +0900

    Merge pull request #3 from devsisters/dashboard_csv_export
    대시보드에서 차트 CSV Export 할 때 internal server error 발생하던 것 수정

--- superset/views/base.py
+++ superset/views/base.py
@@ -37,6 +37,7 @@ from sqlalchemy.orm import Query
 from werkzeug.exceptions import HTTPException
 from wtforms import Form
 from wtforms.fields.core import Field, UnboundField
+from urllib import parse
 
 from superset import (
     app as superset_app,
@@ -138,7 +139,8 @@ def generate_download_headers(
     extension: str, filename: Optional[str] = None
 ) -> Dict[str, Any]:
     filename = filename if filename else datetime.now().strftime("%Y%m%d_%H%M%S")
-    content_disp = f"attachment; filename={filename}.{extension}"
+    filename_quoted = parse.quote(f"{filename}.{extension}")
+    content_disp = f"attachment; filename={filename_quoted}; filename*=utf-8''{filename_quoted}"
     headers = {"Content-Disposition": content_disp}
     return headers
 
@@ -450,7 +452,9 @@ class CsvResponse(Response):  # pylint: disable=too-many-ancestors
     Override Response to take into account csv encoding from config.py
     """
 
-    charset = conf["CSV_EXPORT"].get("encoding", "utf-8")
+    # XXX (kimtkyeom): charset 에서 utf-8-sig를 지원하지 않는 듯 하다. CSV_EXPORT 설정 값을 안쓰고 utf-8로 현재는 고정해둔 상태
+    # charset = conf["CSV_EXPORT"].get("encoding", "utf-8")
+    charset = 'utf-8'
 
 
 def check_ownership(obj: Any, raise_if_false: bool = True) -> bool:
